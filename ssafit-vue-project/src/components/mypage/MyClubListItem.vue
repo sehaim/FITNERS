76499d2d425e7cc294ee936bf54d417758d2243d
sehaim <seha3722@gmail.com>
2024-05-20 18:53:21 +0900

    fix: 오류 수정

---
+++ ssafit-vue-project/src/components/mypage/MyClubListItem.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="my-club-item-container">
+    <div id="club-name">{{ myClub.clubName }}</div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  myClub: Object,
+});
+</script>
+
+<style scoped>
+#my-club-item-container {
+  height: 50px;
+  min-height: 50px;
+  width: 70%;
+  min-width: 300px;
+  max-width: 350px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  box-shadow: 0px 0px 3px 1px #799dbb;
+  margin: 20px 10px;
+  border-radius: 10px;
+  padding: 0 30px;
+}
+
+#club-name {
+  margin: auto;
+  font-size: 16px;
+}
+</style>
--- ssafit-vue-project/src/stores/club.js
+++ ssafit-vue-project/src/stores/club.js
@@ -71,8 +71,6 @@ export const useClubStore = defineStore(
 
     const signupClub = function (clubId) {
       loginUser.value = JSON.parse(localStorage.getItem("loginUser"));
-      console.log(clubId);
-      console.log(loginUser.value);
       axios({
         url: REST_CLUB_API + "/regist",
         method: "POST",
@@ -147,15 +145,15 @@ export const useClubStore = defineStore(
       close,
     };
   },
-  // {
-  //   persist: {
-  //     enabled: true,
-  //     strategies: [
-  //       {
-  //         storage: localStorage,
-  //         paths: ["clubList"],
-  //       },
-  //     ],
-  //   },
-  //}
+  {
+    persist: {
+      enabled: true,
+      strategies: [
+        {
+          storage: localStorage,
+          paths: ["clubList"],
+        },
+      ],
+    },
+  }
 );
