db12a63424869b9000f10ba8e38205e9798c085e
kgc9007 <gic3dev@gmail.com>
2024-05-21 20:45:50 +0900

    test: sample data 추가

--- assets/DB/sampledata_member.sql
+++ assets/DB/sampledata_member.sql
@@ -3,77 +3,45 @@
 -- SAMPLE DATA
 -- -----------------------------------------------------
 
--- 싸피 7반 1 멤버 추가
--- 유저 1 ~ 14 / admin0003
+-- 가입 유저 데이터 추가
 INSERT INTO Member (user_id, club_id, access)
 VALUES 
-("ssafy0001", 1, 1),
-("ssafy0002", 1, 1),
-("ssafy0003", 1, 1),
-("ssafy0004", 1, 1),
-("ssafy0005", 1, 1),
-("ssafy0006", 1, 1),
-("ssafy0007", 1, 1),
-("ssafy0008", 1, 1),
-("ssafy0009", 1, 1),
-("ssafy0010", 1, 1),
-("ssafy0011", 1, 1),
-("ssafy0012", 1, 1),
-("ssafy0013", 1, 1),
-("ssafy0014", 1, 1),
-("admin0003", 1, 1);
+("user0001", 1, 1),
+("user0001", 5, 1),
+("user0002", 4, 1),
+("user0003", 7, 1),
+("user0004", 3, 1),
+("user0005", 5, 1),
+("user0006", 1, 1),
+("user0006", 6, 1),
+("user0007", 2, 1),
+("user0007", 5, 1),
+("user0008", 2, 1),
+("user0008", 7, 1),
+("user0009", 4, 1),
+("user0009", 5, 1),
+("user0010", 5, 1),
+("user0010", 6, 1),
+("user0011", 6, 1),
+("user0011", 7, 1),
+("user0012", 2, 1),
+("user0012", 7, 1);
 
--- 싸피 7반 2 멤버 추가
--- 유저 15 ~ 28 / admin0004
-INSERT INTO Member (user_id, club_id, access)
-VALUES 
-("ssafy0015", 2, 1),
-("ssafy0016", 2, 1),
-("ssafy0017", 2, 1),
-("ssafy0018", 2, 1),
-("ssafy0019", 2, 1),
-("ssafy0020", 2, 1),
-("ssafy0021", 2, 1),
-("ssafy0022", 2, 1),
-("ssafy0023", 2, 1),
-("ssafy0024", 2, 1),
-("ssafy0025", 2, 1),
-("ssafy0026", 2, 1),
-("ssafy0027", 2, 1),
-("ssafy0028", 2, 1),
-("admin0004", 2, 1);
-
-
--- 6630 멤버 추가
-INSERT INTO Member (user_id, club_id, access)
-VALUES
-("ssafy0011", 3, 1),
-("ssafy0017", 3, 1),
-("ssafy0018", 3, 1),
-("ssafy0025", 3, 1),
-("admin0003", 3, 1);
-
--- 아침부터 모하닝 멤버 추가
-INSERT INTO Member (user_id, club_id, access)
-VALUES 
-("ssafy0002", 4, 1),
-("ssafy0005", 4, 1),
-("ssafy0006", 4, 1),
-("ssafy0010", 4, 1),
-("ssafy0015", 4, 1),
-("ssafy0023", 4, 1),
-("admin0004", 4, 1);
-
--- 7반 자치회 멤버 추가
-INSERT INTO Member (user_id, club_id, access)
-VALUES 
-("ssafy0015", 5, 1),
-("ssafy0018", 5, 1),
-("admin0003", 5, 1);
 
 -- 가입 요청중 데이터 추가
 INSERT INTO Member (user_id, club_id, access)
 VALUES 
-("ssafy0003", 3, 0),
-("ssafy0003", 4, 0),
-("ssafy0014", 5, 0);
+("user0001", 7, 0),
+("user0002", 6, 0),
+("user0003", 3, 0),
+("user0004", 5, 0),
+("user0005", 6, 0),
+("user0006", 7, 0),
+("user0007", 6, 0),
+("user0007", 7, 0),
+("user0008", 5, 0),
+("user0008", 6, 0),
+("user0009", 3, 0),
+("user0009", 6, 0),
+("user0011", 3, 0),
+("user0012", 5, 0);
--- assets/DB/sampledata_user.sql
+++ assets/DB/sampledata_user.sql
@@ -3,6 +3,7 @@
 -- SAMPLE DATA
 -- -----------------------------------------------------
 
+-- 일반 사용자 샘플 1
 INSERT INTO User (user_id, user_name, user_password, isManager)
 VALUES 
 ("ssafy0001", "권용수", "ssafy0001@", 0),
@@ -32,8 +33,40 @@ VALUES
 ("ssafy0025", "한도형", "ssafy0025@", 0),
 ("ssafy0026", "한진형", "ssafy0026@", 0),
 ("ssafy0027", "홍주형", "ssafy0027@", 0),
-("ssafy0028", "황민욱", "ssafy0028@", 0),
-("admin0001", "김기창", "admin0001@", 1),
-("admin0002", "임세하", "admin0002@", 1),
-("admin0003", "육예진", "admin0003@", 1),
-("admin0004", "신승호", "admin0004@", 1);
+("ssafy0028", "황민욱", "ssafy0028@", 0);
+
+-- 일반 사용자 샘플 2
+INSERT INTO User (user_id, user_name, user_password, isManager)
+VALUES 
+("user0001", "김서준", "user0001@", 0),
+("user0002", "이수민", "user0002@", 0),
+("user0003", "박민서", "user0003@", 0),
+("user0004", "정예준", "user0004@", 0),
+("user0005", "강하윤", "user0005@", 0),
+("user0006", "조민준", "user0006@", 0),
+("user0007", "윤하린", "user0007@", 0),
+("user0008", "임서진", "user0008@", 0),
+("user0009", "최지호", "user0009@", 0),
+("user0010", "한유준", "user0010@", 0),
+("user0011", "배지안", "user0011@", 0),
+("user0012", "장서현", "user0012@", 0);
+
+
+-- 매니저 샘플 1
+INSERT INTO User (user_id, user_name, user_password, isManager)
+VALUES 
+("admin0001", "김민준", "admin0001@", 1),
+("admin0002", "이상민", "admin0002@", 1),
+("admin0003", "박지훈", "admin0003@", 1),
+("admin0004", "정연우", "admin0004@", 1),
+("admin0005", "김하린", "admin0005@", 1),
+("admin0006", "이도윤", "admin0006@", 1),
+("admin0007", "박준서", "admin0007@", 1),
+("admin0008", "한서현", "admin0008@", 1),
+("admin0009", "김다은", "admin0009@", 1),
+("admin0010", "최유진", "admin0010@", 1),
+("admin0011", "오승준", "admin0011@", 1),
+("admin0012", "임하린", "admin0012@", 1),
+("admin0013", "신지후", "admin0013@", 1),
+("admin0014", "문수아", "admin0014@", 1),
+("admin0015", "정하윤", "admin0015@", 1);
