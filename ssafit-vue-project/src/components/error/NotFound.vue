53c46d44e4ba1bac08af2a33e99538acd75baa1e
sehaim <seha3722@gmail.com>
2024-05-23 17:15:33 +0900

    fix: NotFound.vue file name

---
+++ ssafit-vue-project/src/components/error/NotFound.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="not-found-container">
+    <img src="@/assets/img/sad-icon.png" alt="sad-icon" />
+    <div id="text">잘못된 접근입니다!</div>
+  </div>
+</template>
+
+<style scoped>
+#not-found-container {
+  width: 500px;
+  height: 500px;
+  margin: auto;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-250px, -250px);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  padding: 180px 0;
+}
+
+img {
+  width: 65px;
+  opacity: 70%;
+}
+
+#text {
+  font-size: 20px;
+}
+</style>
